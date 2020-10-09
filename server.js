4380c078e5ec65a44edbefe88d72e61cc524fd8d
Shin Maung Maung <ethereal97@gmail.com>
2020-10-09 19:17:51 +0630

    (no message)

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+// const socket = require('socket.io');
+const path = require('path');
+
+const app = express();
+
+let public_path = path.join(__dirname, 'public');
+let port = process.env.PORT || 3000;
+
+app.use(express.static(public_path));
+
+app.listen(port);
+
+//
